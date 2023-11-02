58dfc7508b70a4a63f0b8ecb2e8678951f2ed2ca
brittany duncan <duncandakota08@gmail.com>
2023-11-02 16:42:36 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: `Sam`,
+    streetAdress: '11 Broadway'
+}
+
+
+
+
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    const newObj = Object.assign({}, employeeObject, {[key]: value});
+    return newObj;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    employeeObject[key] = value;
+    return employeeObject;
+  }
+  
+  function deleteFromEmployeeByKey(employeeObject, key) {
+    const newObj = Object.assign({}, employeeObject);
+    delete newObj[key];
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    delete employeeObject[key];
+    return employeeObject;
+  }
